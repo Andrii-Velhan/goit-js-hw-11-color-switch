079114c37ba897a238761d40f43a265a9fed8336
Andrii-Velhan <velganaa@gmail.com>
2020-11-04 22:43:41 +0200

    dell unUsing folders & files

--- src/modules/variables.js
+++
@@ -1,4 +0,0 @@
-export const startBtn = document.querySelector("[data-action='start']");
-export const stopBtn = document.querySelector("[data-action='stop']");
-export const bodyEl = document.querySelector('body');
-
