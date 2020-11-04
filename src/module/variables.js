6034820d61c5c804caecebc1e2d686989c646de8
Andrii-Velhan <velganaa@gmail.com>
2020-11-04 21:14:51 +0200

    clone Webpack; add input data

---
+++ src/module/variables.js
@@ -0,0 +1,4 @@
+export const menuContainer = document.querySelector('.js-menu');
+export const BtnSwitch = document.querySelector('input.js-switch-input');
+export const bodyEl = document.querySelector('body');
+
